10dabd00bc9157d229fe266ed2c009755fba257b
Matt Keas <matthiasak@gmail.com>
2016-11-03 09:58:39 -0500

    adding sessions

--- Controllers/CRUD.cs
+++ Controllers/CRUD.cs
@@ -7,14 +7,14 @@ using System;
 
 RESTful takes these endpoints for each resource
 
-API	                    Description	            Request body	        Response body
+API                     Description             Request body            Response body
 --------------------------------------------------------------------------------------------
-GET /api/todo	        Get all to-do items	    None	                Array of to-do items
-GET /api/todo/{id}	    Get an item by ID	    None	                To-do item
-POST /api/todo	        Add a new item	        To-do item	            To-do item
-PUT /api/todo/{id}	    Update an existing item	To-do item (full)       None
-PATCH /api/todo/{id}	Update an existing item	To-do item (partial)	None
-DELETE /api/todo/{id}	Delete an item.	        None	                None
+GET /api/todo           Get all to-do items     None                    Array of to-do items
+GET /api/todo/{id}      Get an item by ID       None                    To-do item
+POST /api/todo          Add a new item          To-do item              To-do item
+PUT /api/todo/{id}      Update an existing item To-do item (full)       None
+PATCH /api/todo/{id}    Update an existing item To-do item (partial)    None
+DELETE /api/todo/{id}   Delete an item.         None                    None
 
 */
 public abstract class CRUDController<T> : Controller where T: class, HasId
@@ -26,21 +26,21 @@ public abstract class CRUDController<T> : Controller where T: class, HasId
 
     [HttpPost]
     public IActionResult C([FromBody] T item) {
-        if(!ModelState.IsValid) 
+        if(!ModelState.IsValid)
             return BadRequest(ModelState.ToErrorObject());
-            
+
         return Ok(r.Create(item));
     }
 
-    [HttpGet("{id?}")]
-    public IActionResult R(int id = -1) {
-        if(id == -1)
-            return Ok(r.Read());
-        
+    [HttpGet]
+    public IActionResult R() => Ok(r.Read());
+
+    [HttpGet("{id}")]
+    public IActionResult R(int id) {
         var item = r.Read(id);
         if(item == null)
             return NotFound();
-        
+
         return Ok(item);
     }
 
@@ -49,7 +49,7 @@ public abstract class CRUDController<T> : Controller where T: class, HasId
         if(item.Id != id || !ModelState.IsValid || !r.Update(item))
             return BadRequest();
 
-        return Ok(); 
+        return Ok();
     }
 
     [HttpDelete("{id}")]
@@ -57,7 +57,7 @@ public abstract class CRUDController<T> : Controller where T: class, HasId
         T item = r.Delete(id);
         if(item == null)
             return NotFound();
-        
+
         return Ok(item);
     }
 }
